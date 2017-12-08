4b520e3ef17249934fab8447663f55a0d6db7f4e
Garis <silegagp3@gmail.com>
2017-12-07 20:40:35 -0500

    no more nullpointer when typing albumname

--- app/src/main/java/cs213/androidphotos/MainActivity.java
+++ app/src/main/java/cs213/androidphotos/MainActivity.java
@@ -3,29 +3,33 @@ package cs213.androidphotos;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.app.AlertDialog;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
     private static Button button;
     private EditText addAlbum;
+    private TextView trying;
+    String alb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        System.out.println("LOL");
         CreateAlbum();
 
     }
 
     public void CreateAlbum(){
         button=  findViewById(R.id.buttonCreate);
-        addAlbum = findViewById(R.id.editTextAlbumInput);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
                 View promptsView = li.inflate(R.layout.createalert,null);
                 AlertDialog.Builder a = new AlertDialog.Builder(MainActivity.this);
                 a.setView(promptsView);
+                addAlbum = (EditText) promptsView.findViewById(R.id.editTextAlbumInput);
+                trying = promptsView.findViewById(R.id.CreateAlbumTitle);
+                System.out.println(addAlbum + "addAlbum");
+                System.out.println(trying + "trying");
                 final EditText userInput = (EditText) promptsView
                         .findViewById(R.id.editTextAlbumInput);
                 a.setMessage("Input new Album Name")
@@ -40,9 +48,13 @@ public class MainActivity extends AppCompatActivity {
                         .setPositiveButton("Add",new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                addAlbum.setText(userInput.getText());
+                               addAlbum.setText(userInput.getText());
+                                alb = addAlbum.getText().toString();
+                                System.out.println(alb);
+                                finish();
                             }
-                        })
+                        }
+                        )
                         .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
